408099e4e02ff286fdd6033c0cff5564de1bdf97
Hayder <hayder.alhatemi@gmail.com>
2025-06-09 17:44:46 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector('LoanForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const amount = parseInt(document.getElementById('amount').value);
+    const consent = document.getElementById('consent').checked;
+
+    if 
+});
